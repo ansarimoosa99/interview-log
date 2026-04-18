282d920ccafd2f7b9f3b522cff2fa0ff15b38a1d
Moosa Ansari <ansarimoosa99@gmail.com>
2026-04-19 05:16:45 +0530

    SPD-10: Handle validation errors with meaningful messages

--- src/main/java/com/moosa/interviewlog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/moosa/interviewlog/exception/GlobalExceptionHandler.java
@@ -2,15 +2,21 @@ package com.moosa.interviewlog.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleRuntime(RuntimeException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidation(MethodArgumentNotValidException ex) {
+
+    String error = ex.getBindingResult()
+            .getFieldError()
+            .getDefaultMessage();
+
+    return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+}
     
 }
