2c4e838f2674d6e4037a7f3e0a066a2a18d7d7c8
Moosa Ansari <ansarimoosa99@gmail.com>
2026-04-18 22:37:47 +0530

    SPD-4: add README and initial project setup

---
+++ src/main/java/com/moosa/interviewlog/InterviewlogApplication.java
@@ -0,0 +1,13 @@
+package com.moosa.interviewlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moosa/interviewlog/InterviewlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moosa.interviewlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
